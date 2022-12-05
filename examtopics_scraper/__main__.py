b44ded18ad1a71dbc4aabc6fc8aa77ed22b5ccbf
Alessandro Serpi <alessandro.serpi@me.com>
2022-12-05 23:16:42 +0100

    Add main

---
+++ examtopics_scraper/__main__.py
@@ -0,0 +1,19 @@
+import argparse
+import sys
+
+from examtopics_scraper.scrapers import scrape_exams, scrape_questions
+
+parser = argparse.ArgumentParser()
+parser.add_argument("provider")
+parser.add_argument("-e", "--exam", help="exam code")
+parser.add_argument("-o", "--output", help="output path")
+parser.add_argument("-v", "--verbose", action="store_true", help="enable debug logging")
+args = parser.parse_args()
+
+if args.exam:
+    scrape_questions(args.provider, args.exam, args.output, args.verbose)
+
+if args.output:
+    print("Cannot save output of provider scraper.")
+    sys.exit(64)
+scrape_exams(args.provider, args.verbose)
---
+++ examtopics_scraper/scrapers.py
@@ -0,0 +1,6 @@
+def scrape_exams(provider: str, verbose: bool = False):
+    raise NotImplementedError
+
+
+def scrape_questions(provider: str, exam: str, output: str, verbose: bool = False):
+    raise NotImplementedError
