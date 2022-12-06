4f6df0f848ad1c79385debb6f7d31a85c112d141
Alessandro Serpi <alessandro.serpi@me.com>
2022-12-06 11:12:27 +0100

    Fix flow of operations

--- examtopics_scraper/__main__.py
+++ examtopics_scraper/__main__.py
@@ -4,7 +4,7 @@ import sys
 from examtopics_scraper.scrapers import scrape_exams, scrape_questions
 
 parser = argparse.ArgumentParser()
-parser.add_argument("provider")
+parser.add_argument("provider", help="exam provider")
 parser.add_argument("-e", "--exam", help="exam code")
 parser.add_argument("-o", "--output", help="output path")
 parser.add_argument("-v", "--verbose", action="store_true", help="enable debug logging")
@@ -12,8 +12,8 @@ args = parser.parse_args()
 
 if args.exam:
     scrape_questions(args.provider, args.exam, args.output, args.verbose)
-
-if args.output:
+elif args.output:
     print("Cannot save output of provider scraper.")
     sys.exit(64)
-scrape_exams(args.provider, args.verbose)
+else:
+    scrape_exams(args.provider, args.verbose)
