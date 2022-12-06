3cc941c0154a8f76bc6e71b76731179ca6d2315d
Alessandro Serpi <alessandro.serpi@me.com>
2022-12-06 10:19:07 +0100

    Add exporters

--- examtopics_scraper/exporters.py
+++ examtopics_scraper/exporters.py
@@ -1,12 +1,39 @@
+import itemadapter
+
+
 class ExamtopicsExamsExportPipeline:
     def process_item(self, item, spider):
-        raise NotImplementedError
+        dict_item = itemadapter.ItemAdapter(item).asdict()
+        print(f"{dict_item['code']}{dict_item['name']}")
+        return item
 
 
 class ExamtopicsQuestionsExportPipeline:
     def process_item(self, item, spider):
-        raise NotImplementedError
+        print(itemadapter.ItemAdapter(item).asdict()['url'])
+        return item
 
 
 def generate_questions_html_exporter(provider: str, exam: str, output: str):
-    raise NotImplementedError
+    class ExamtopicsQuestionsHtmlExportPipeline:
+        def __init__(self):
+            self.questions = []
+
+        def close_spider(self, spider):
+            with open(output, "w") as out:
+                print("<html><head><style>table{border-collapse:collapse;font-family:Arial,Helvetica,sans-serif;width:100%}td,th{border:1px solid #ddd;padding:8px}table tr:nth-child(2n){background-color:#f2f2f2}table tr:hover{background-color:#ddd}table th{background-color:#0095eb;color:#fff;padding-bottom:12px;padding-top:12px;text-align:left}</style></head>"
+                      f"<body><h1>{provider} {exam}</h1><table><"
+                      f"tr><th>Topic</th><th>Question</th></tr>", file=out)
+                for question in sorted(self.questions, key=lambda q: (q["topic"], q["question"])):
+                    print(f'<tr><td>{question["topic"]}</td><td><a href="{question["url"]}" '
+                          f'target="_blank">{question["question"]}</a></td></tr>', file=out)
+                print("</table></body></html>", file=out)
+
+        def open_spider(self, spider):
+            self.questions = []
+
+        def process_item(self, item, spider):
+            self.questions.append(itemadapter.ItemAdapter(item).asdict())
+            return item
+
+    return ExamtopicsQuestionsHtmlExportPipeline
